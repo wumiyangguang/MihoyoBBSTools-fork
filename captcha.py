e2ea4deb0e571fc54253f8b72d119ef2a29c68d4
wumiyangguang <96178242+wumiyangguang@users.noreply.github.com>
2024-09-20 19:25:11 +0800

    更新 captcha.py
    添加支持人人码

--- captcha.py
+++ captcha.py
@@ -1,9 +1,74 @@
-from request import http
+from request import http,TimeoutError,RequestError
+import setting
+from loghelper import log
+import time
 
+api_url='http://api.rrocr.com/api/recognize.html' #接口地址
+appkey = '###################'       #接口appkey
+
+'''
+def post_with_retry(api_url: str,data: dict,timeout=10,retry_delay = 5):
+    while True:
+        try:
+            response = http.post(api_url,data=data,timeout=timeout)
+            response.raise_for_status()  # 如果返回的状态码不是200，抛出HTTPError异常
+            result = response.json()
+            return result # 请求成功，返回response对象
+        except TimeoutError:
+            log.warning("请求超时，正在重试...")
+        except RequestError as e:
+            log.warning(f"请求失败：{e}")
+        except Exception as e:
+            log.warning(f"未知错误：{e}")
+            return None  # 如果是其他请求错误，退出循环
+        time.sleep(retry_delay)  # 等待重试延迟时间
+        '''
 
 def game_captcha(gt: str, challenge: str):
-    return None  # 失败返回None 成功返回validate
-
+    data = {
+    'appkey': appkey,
+    'gt': gt,
+    'challenge': challenge,
+    'referer': setting.cn_game_is_signurl,
+    'ip': '',
+    'host': ''
+}
+    try:
+        response = http.post(api_url,data=data)
+        result = response.json()
+        if result.get("status") == 0 and result.get("msg") == "识别成功":
+            validate = result["data"].get("validate")
+            return validate
+        else:
+        # 识别失败，返回None或其他提示
+        # log.warning(f"{result.get('msg')}")
+            return None
+    except:
+        return None
 
 def bbs_captcha(gt: str, challenge: str):
-    return None
+    data = {
+    'appkey': appkey,
+    'gt': gt,
+    'challenge': challenge,
+    'referer': setting.bbs_get_captcha,
+    'ip': '',
+    'host': ''
+}
+    try:
+        response = http.post(api_url,data=data)
+        result = response.json()
+        if result.get("status") == 0 and result.get("msg") == "识别成功":
+            validate = result["data"].get("validate")
+            return validate
+        else:
+        # 识别失败，返回None或其他提示
+        # log.warning(f"{result.get('msg')}")
+            return None
+    except:
+        return None
+
+
+if __name__ == "__main__":
+    test=game_captcha(gt='d019a22590a54475b8e30eeb2854aab9',challenge='92e65680ceb0554c2410eac3cdcde3d5')
+    #print(test.log)
