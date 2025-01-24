22eefa4da79a19976a6c5d978234e377d16b2917
wumiyangguang <96178242+wumiyangguang@users.noreply.github.com>
2025-01-24 19:49:46 +0800

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -34,15 +34,14 @@ def post_with_retry(api_url: str,data: dict,timeout=10,retry_delay = 5):
 
 def game_captcha(gt: str, challenge: str,header: dict):
     #print(header)
-    headers = urllib.parse.quote(header.get('User-Agent'))
+    #headers = urllib.parse.quote(header.get('User-Agent'))
     data = {
     'appkey': appkey,
     'gt': gt,
     'challenge': challenge,
     'referer': setting.cn_game_sign_url,
     'ip': '',
-    'host': '',
-    'useragent': headers
+    'host': ''
 }
     try:
         response = http.post(api_url,data=data)
@@ -60,15 +59,14 @@ def game_captcha(gt: str, challenge: str,header: dict):
         return None
 
 def bbs_captcha(gt: str, challenge: str,header: dict):
-    headers = urllib.parse.quote(header.get('User-Agent'))
+    #headers = urllib.parse.quote(header.get('User-Agent'))
     data = {
     'appkey': appkey,
     'gt': gt,
     'challenge': challenge,
     'referer': setting.bbs_get_captcha,
     'ip': '',
-    'host': '',
-    'useragent': headers
+    'host': ''
 }
     try:
         response = http.post(api_url,data=data)
